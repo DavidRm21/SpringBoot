2400f39aa234de9c3db21d96d071b4a79e287bd6
Cristian Romero <cdavidromero214@gmail.com>
2024-12-18 19:53:31 -0500

    servicio de eureka server

---
+++ mobile-app-eureka/src/test/java/com/course/mobile_app_eureka/MobileAppEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.mobile_app_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
